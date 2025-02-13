a844b1604035826a2af19ab4bc46ff3a21822dbb
Sora Yamamoto <yanagidalab@outlook.com>
2025-02-14 08:56:58 +0900

    クラウド環境で COM7 エラーを防ぐ修正

--- co2_sensor_app/pages/03_pump_check_page.py
+++ co2_sensor_app/pages/03_pump_check_page.py
@@ -10,19 +10,23 @@ pump2_flow_rate = st.slider("Pump 2 Flow Rate", min_value=1, max_value=60, value
 # Pump control buttons
 if st.button("Pump 1 ON"):
     TxData = f"TP11{pump1_flow_rate}\r"  # Dynamic flow rate selection
-    st.session_state.ser.write(TxData.encode("utf-8"))
+    if st.session_state.ser:  # シリアルポートが開いている場合のみ送信
+        st.session_state.ser.write(TxData.encode("utf-8"))
 
 if st.button("Pump 1 OFF"):
     TxData = "TP10\r"  # Stop pump 1
-    st.session_state.ser.write(TxData.encode("utf-8"))
+    if st.session_state.ser:  # シリアルポートが開いている場合のみ送信
+        st.session_state.ser.write(TxData.encode("utf-8"))
 
 if st.button("Pump 2 ON"):
     TxData = f"TP21{pump2_flow_rate}\r"  # Dynamic flow rate selection
-    st.session_state.ser.write(TxData.encode("utf-8"))
+    if st.session_state.ser:  # シリアルポートが開いている場合のみ送信
+        st.session_state.ser.write(TxData.encode("utf-8"))
 
 if st.button("Pump 2 OFF"):
     TxData = "TP20\r"  # Stop pump 2
-    st.session_state.ser.write(TxData.encode("utf-8"))
+    if st.session_state.ser:  # シリアルポートが開いている場合のみ送信
+        st.session_state.ser.write(TxData.encode("utf-8"))
 
 # Flash memory delete function
 if st.button("Delete Flash Memory"):
